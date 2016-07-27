73b64bc9dfd26ba0b71dd8d0bf56701f875aea86
Katie Vogel <katiebvogel@gmail.com>
2016-07-27 10:03:59 -0500

    adding a button

--- client.js
+++ client.js
@@ -6,15 +6,19 @@ angular.module('mapApp').controller('MainController', function($http){
 
   var vm = this;
 
-  vm.message = 'Maps are the best!'; 
+  vm.message = 'Maps are the best!';
 
-  function initMap(){
-
-  vm.map = new google.maps.Map(document.getElementById('vm.map'), {
+  vm.initMap = function(){
+    var sendData = {};
+    sendData.task = vm.tempTask;
+    
+  vm.map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 44.9778, lng: -93.2650},
       zoom: 8
     });
   }
+
+
 var drawingManager = new google.maps.drawing.DrawingManager({
   drawingMode: google.maps.drawing.OverlayType.MARKER,
   drawingControl: true,
